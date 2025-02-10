9920822a9054d0fe74b2dea492ac7ef0e6622eee
Melih <mustafamelihcetin@outlook.com>
2025-02-10 23:38:54 +0300

    AppDbContext.

---
+++ Tazq-App/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tazq_App.Models;
+
+namespace Tazq_App.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+		public DbSet<TaskItem> Tasks { get; set; }
+		public DbSet<User> Users { get; set; }
+	}
+}
