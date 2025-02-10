30cea42138eaa181dc147a687741555152896934
Melih <mustafamelihcetin@outlook.com>
2025-02-10 23:28:26 +0300

    TaskItem Model

---
+++ Tazq-App/Models/TaskItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Tazq_App.Models
+{
+	public class TaskItem
+	{
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public TaskPriority Priority { get; set; } = TaskPriority.Medium;
+    }
+    public enum TaskPriority
+    {
+        Low,
+        Medium,
+        High
+    }
+}
