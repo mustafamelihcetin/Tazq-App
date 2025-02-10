01f2025d870ff0e1ef36f9a372afd191fa7aa260
Melih <mustafamelihcetin@outlook.com>
2025-02-10 23:30:18 +0300

    User Model.

---
+++ Tazq-App/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Tazq_App.Models
+{
+	public class User
+	{
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public List<TaskItem> Tasks { get; set; }
+    }
+}
