03f39fb4fbef667167df347c09a5b8843866ea32
Melih <mustafamelihcetin@outlook.com>
2025-02-21 06:34:06 +0300

    The frontend phase of the software project was created.

---
+++ Tazq-App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Tazq_App
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ Tazq-App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Tazq_App
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
