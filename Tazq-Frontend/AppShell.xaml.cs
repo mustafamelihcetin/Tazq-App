ff2af5b45b3a0529faef773f23d29993107fe1ed
Melih <mustafamelihcetin@outlook.com>
2025-02-23 23:48:35 +0300

    Fixed bugs.

---
+++ Tazq-Frontend/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Tazq_Frontend
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Tazq-Frontend/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Tazq_Frontend;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
