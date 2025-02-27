a8652acfd9c71da1a364bf1024f7f083c5675471
Melih <mustafamelihcetin@outlook.com>
2025-02-27 13:25:54 +0300

    .

--- Tazq-Frontend/Views/LoginPage.xaml.cs
+++ Tazq-Frontend/Views/LoginPage.xaml.cs
@@ -1,5 +1,11 @@
+using Microsoft.Maui.Platform;
 using Tazq_Frontend.ViewModels;
 using Microsoft.Maui.Controls;
+#if WINDOWS
+using Microsoft.UI.Windowing;
+using Microsoft.UI;
+using Windows.Graphics;
+#endif
 
 namespace Tazq_Frontend.Views
 {
@@ -12,6 +18,40 @@ namespace Tazq_Frontend.Views
 			InitializeComponent();
 			_viewModel = new AuthViewModel();
 			BindingContext = _viewModel;
+
+			SetWindowSize(); // Windows için minimum pencere boyutunu ayarla
+		}
+
+		// Windows için minimum pencere boyutunu belirleme
+		private void SetWindowSize()
+		{
+#if WINDOWS
+            var window = this.GetParentWindow();
+            if (window?.Handler?.PlatformView is Microsoft.UI.Xaml.Window nativeWindow)
+            {
+                IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+                var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+                var appWindow = AppWindow.GetFromWindowId(windowId);
+
+                if (appWindow != null)
+                {
+                    // Baþlangýç boyutunu belirle
+                    appWindow.Resize(new SizeInt32(1024, 768));
+
+                    // Minimum pencere boyutunu belirle (Küçültmeyi engelle)
+                    var presenter = appWindow.Presenter as OverlappedPresenter;
+                    if (presenter != null)
+                    {
+                        presenter.SetBorderAndTitleBar(true, true);
+                        presenter.IsResizable = true; // Kullanýcý yine boyutlandýrabilir ama min sýnýrý olur
+                        presenter.Maximize(); // Pencereyi baþlarken maksimum boyutta aç
+                    }
+
+                    // Minimum boyutu doðrudan ayarla
+                    appWindow.Resize(new SizeInt32(Math.Max(appWindow.Size.Width, 800), Math.Max(appWindow.Size.Height, 600)));
+                }
+            }
+#endif
 		}
 
 		// Google Sign-In Click Event
