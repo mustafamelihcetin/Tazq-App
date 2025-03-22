ba6cc18263de9f64867f17225963f57553b07fb8
Melih <mustafamelihcetin@outlook.com>
2025-03-22 05:42:08 +0300

    JsonPropertyName in UserRegisterDto

--- Tazq-Backend/Models/UserRegisterDto.cs
+++ Tazq-Backend/Models/UserRegisterDto.cs
@@ -1,16 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Tazq_App.Models
 {
 	public class UserRegisterDto
 	{
 		[Required, EmailAddress]
+		[JsonPropertyName("email")]
 		public string Email { get; set; } = string.Empty;
 
 		[Required]
+		[JsonPropertyName("name")]
 		public string Name { get; set; } = string.Empty; // Full name in one field
 
 		[Required, MinLength(6)]
+		[JsonPropertyName("password")]
 		public string Password { get; set; } = string.Empty;
 	}
 
