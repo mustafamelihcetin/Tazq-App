5955a7ff04fe1f79f150e79edf6345a83e9bff73
Melih <mustafamelihcetin@outlook.com>
2025-03-26 03:44:57 +0300

    ...

--- Tazq-Frontend/AppShell.xaml.cs
+++ Tazq-Frontend/AppShell.xaml.cs
@@ -13,6 +13,7 @@ namespace Tazq_Frontend
 			Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
 			Routing.RegisterRoute(nameof(RegisterPage), typeof(RegisterPage));
 			Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
+			Routing.RegisterRoute(nameof(AddTaskPage), typeof(AddTaskPage));
 		}
 	}
 }
