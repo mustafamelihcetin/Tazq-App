89c161f171039eb0b9ca0be9ed6e511fdfa2abde
Melih <mustafamelihcetin@outlook.com>
2025-04-04 01:42:13 +0300

    LoginPage: Add spinner inside login button and trigger login on Enter key; disable all inputs during loading.

--- Tazq-Frontend/Views/LoginPage.xaml.cs
+++ Tazq-Frontend/Views/LoginPage.xaml.cs
@@ -1,29 +1,39 @@
 using Tazq_Frontend.ViewModels;
 using Microsoft.Maui.Controls;
+using System;
 
 namespace Tazq_Frontend.Views
 {
-	public partial class LoginPage : ContentPage
-	{
-		private readonly AuthViewModel _viewModel;
+    public partial class LoginPage : ContentPage
+    {
+        private readonly AuthViewModel _viewModel;
 
-		public LoginPage()
-		{
-			InitializeComponent();
-			_viewModel = new AuthViewModel();
-			BindingContext = _viewModel;
-		}
+        public LoginPage()
+        {
+            InitializeComponent();
+            _viewModel = new AuthViewModel();
+            BindingContext = _viewModel;
+        }
 
-		// Google Sign-In Click Event
-		private async void OnGoogleLoginClicked(object sender, EventArgs e)
-		{
-			await DisplayAlert("Google Giriþ", "Google ile giriþ iþlemi henüz entegre edilmedi.", "Tamam");
-		}
+        // Trigger login on Enter key press in password field
+        private void OnPasswordCompleted(object sender, EventArgs e)
+        {
+            if (_viewModel.LoginCommand.CanExecute(null))
+            {
+                _viewModel.LoginCommand.Execute(null);
+            }
+        }
 
-		// Apple Sign-In Click Event
-		private async void OnAppleLoginClicked(object sender, EventArgs e)
-		{
-			await DisplayAlert("Apple Giriþ", "Apple ile giriþ iþlemi henüz entegre edilmedi.", "Tamam");
-		}
-	}
+        // Google Sign-In Click Event
+        private async void OnGoogleLoginClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Google Giriþ", "Google ile giriþ iþlemi henüz entegre edilmedi.", "Tamam");
+        }
+
+        // Apple Sign-In Click Event
+        private async void OnAppleLoginClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Apple Giriþ", "Apple ile giriþ iþlemi henüz entegre edilmedi.", "Tamam");
+        }
+    }
 }
