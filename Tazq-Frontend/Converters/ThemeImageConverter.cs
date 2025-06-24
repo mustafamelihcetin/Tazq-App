838e1b748694911ab0f986ec03315dcb32fed176
melih <mustafamelihcetin@outlook.com>
2025-06-24 23:59:14 +0300

    ...

---
+++ Tazq-Frontend/Converters/ThemeImageConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace Tazq_Frontend.Converters
+{
+    public class ThemeImageConverter : IValueConverter
+    {
+        public string Light { get; set; }
+        public string Dark { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var currentTheme = Application.Current?.RequestedTheme ?? AppTheme.Light;
+
+            return currentTheme == AppTheme.Dark ? Dark : Light;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tazq-Frontend/Helpers/ThemedImageExtension.cs
@@ -0,0 +1,28 @@
+﻿// This markup extension selects image based on current app theme.
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using System;
+
+namespace Tazq_Frontend.Helpers
+{
+    public class ThemedImageExtension : IMarkupExtension<ImageSource>
+    {
+        public string Light { get; set; }
+        public string Dark { get; set; }
+
+        public ImageSource ProvideValue(IServiceProvider serviceProvider)
+        {
+            var theme = Application.Current?.RequestedTheme ?? AppTheme.Unspecified;
+
+            return theme switch
+            {
+                AppTheme.Light => Light,
+                AppTheme.Dark => Dark,
+                _ => Light
+            };
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+            => ProvideValue(serviceProvider);
+    }
+}
