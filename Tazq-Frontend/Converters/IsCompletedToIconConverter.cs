280fd52f7303fb868e4873d1e0a2a43216e16f47
melih <mustafamelihcetin@outlook.com>
2025-06-27 11:30:26 +0300

    ...

--- Tazq-Frontend/Converters/IsCompletedToIconConverter.cs
+++ Tazq-Frontend/Converters/IsCompletedToIconConverter.cs
@@ -7,24 +7,19 @@ namespace Tazq_Frontend.Converters
     public class IsCompletedToIconConverter : IValueConverter
     {
         // Converts true/false to theme aware checkmark or empty box icon
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            bool isLightTheme = Application.Current.UserAppTheme == AppTheme.Unspecified
-               ? Application.Current.RequestedTheme == AppTheme.Light
-               : Application.Current.UserAppTheme == AppTheme.Light;
-
-            string suffix = isLightTheme ? "_light.png" : "_dark.png";
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {           
 
             if (value is bool isCompleted)
             {
                 return isCompleted
-                    ? $"check_filled{suffix}"
-                    : $"check_outline{suffix}";
+                    ? "check_filled.png"
+                    : "check_outline.png";
             }
-            return $"check_outline{suffix}";
+            return "check_outline.png";
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
             throw new NotImplementedException();
     }
 }
