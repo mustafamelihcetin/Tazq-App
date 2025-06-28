0fb6c5ea5171f2674d03b6aee1778ec46aaf21f7
melih <mustafamelihcetin@outlook.com>
2025-06-28 23:55:31 +0300

    Designing.

--- Tazq-Frontend/Views/AboutPage.xaml.cs
+++ Tazq-Frontend/Views/AboutPage.xaml.cs
@@ -8,4 +8,9 @@ public partial class AboutPage : ContentPage
     {
         InitializeComponent();
     }
+
+    private async void OnBackClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("..");
+    }
 }
